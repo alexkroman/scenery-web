f60a464d7daf803bf16cf9ddddf45a79996fc4af
Alex Kroman <alexkroman@gmail.com>
2012-04-18 11:44:15 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sceneryweb::Application.config.secret_token = 'a7002ff0bdcfd00f9528bf93d5c42a9832be31350c834d29beba2b3b9356d56d6de2e0a48ef61f94e7a4d44890030f318e6e44bbe753a193941290e568f50354'
