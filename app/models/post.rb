1d9910e4361429c6a07bff940ee7396e7643621e
Alex Kroman <alexkroman@gmail.com>
2012-04-20 14:41:59 -0700

    updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :photo
   has_attached_file :photo, 
-  :styles => { :medium => "300x800>" },
-  :convert_options => { :all => '-auto-orient' },
+  :styles => { :original => '5000x5000>', :medium => "600x600>", :small => "320x320>", :thumb => "100x100#" },
+  :processors => [:auto_orient, :thumbnail],  
   :storage => :s3,
       :bucket => ENV['S3_BUCKET_NAME'],
       :s3_credentials => {
