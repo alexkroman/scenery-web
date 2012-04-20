f7427d213d096535db58c065a62976def050609e
Alex Kroman <alexkroman@gmail.com>
2012-04-20 09:50:41 -0700

    updates

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ end
 gem 'jquery-rails'
 gem 'html5-rails'
 gem 'paperclip'
+gem 'aws-sdk', '~> 1.3.4'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :photo
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  :storage => :s3,
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_credentials => {
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
   validates_attachment_presence :photo
 end
