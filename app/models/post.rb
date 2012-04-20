4f581354650ab1aeb03e7b6c5ae445a2819a3a20
Alex Kroman <alexkroman@gmail.com>
2012-04-20 09:55:50 -0700

    updatE

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :photo
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  has_attached_file :photo, 
+  :styles => { :medium => "300x300>" },
   :storage => :s3,
       :bucket => ENV['S3_BUCKET_NAME'],
       :s3_credentials => {
