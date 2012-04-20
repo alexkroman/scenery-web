086acef9979c4971cf66c937544dfe637761116d
Alex Kroman <alexkroman@gmail.com>
2012-04-20 10:05:12 -0700

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ActiveRecord::Base
   :storage => :s3,
       :bucket => ENV['S3_BUCKET_NAME'],
       :s3_credentials => {
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        :access_key_id => ENV['S3_KEY'],
+        :secret_access_key => ENV['S3_SECRET']
       }
   validates_attachment_presence :photo
 end
